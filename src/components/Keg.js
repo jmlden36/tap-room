9fb2bdadf86c9283ed345a1d0f603acb2883ca0e
Matt Luker <jamesmattluker@gmail.com>
2022-02-11 15:30:22 -0700

    add conditional in Keg.js to make sell button go away when keg is empty

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,6 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Keg(props){
+  if (props.pintsLeft <= 0) {
+    return (
+      <React.Fragment>
+        <div >
+          <h2>Pints left: SOLD OUT</h2>
+          <h3>{props.brand} - {props.name}</h3>
+          <h4>Price: $ {props.price}</h4>
+          <h4>Alcohol Content: {props.alcoholContent}%</h4>
+          <button onClick={() => props.whenKegClicked(props.id)}>View Details</button>
+        </div>
+      </React.Fragment>
+    );
+  }
   return (
     <React.Fragment>
       <div >
@@ -14,6 +27,7 @@ function Keg(props){
       </div>
     </React.Fragment>
   );
+  
 }
 
 Keg.propTypes = {
