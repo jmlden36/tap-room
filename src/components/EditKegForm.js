7d640aae3b7a752f5572986271f38e92d3794ce4
Matt Luker <jamesmattluker@gmail.com>
2022-02-11 11:09:15 -0700

    add EditKegForm

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditKegForm (props) {
+  const { keg } = props;
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, pintsLeft: event.target.pintsLeft.value, id: ticket.id});
+  }
+  
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditKegFormSubmission}
+        buttonText="Update Keg Information" />
+    </React.Fragment>
+  );
+}
+
+EditKegForm.propTypes = {
+  keg: PropTypes.object,
+  onEditKeg: PropTypes.func
+};
+
+export default EditKegForm;
