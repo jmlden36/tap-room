3854656ecb39c4b45e8692fa5c1cef6d08a3be2d
Matt Luker <jamesmattluker@gmail.com>
2022-02-11 16:57:40 -0700

    style heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+const header = {
+  color: 'white',
+  textAlign: 'center',
+  textShadow: '3px 2px black'
+}
+
 function Header () {
   return (
     <React.Fragment>
-      <h1>Tap Room</h1>
+      <h1 style={header}>The Grey Toad Tap Room</h1>
     </React.Fragment>
   );
 }
--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 const detailCard = {
   border: '2px solid black',
   color: 'black',
